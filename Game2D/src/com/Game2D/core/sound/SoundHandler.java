34d9b05ff5e335228dd44918c654d39785b3151b
Anttoni <twistedmind123123@gmail.com>
2013-05-28 20:41:17 +0300

    SoundHandler updated + Basic LogHandler

--- Game2D/src/com/Game2D/core/sound/SoundHandler.java
+++ Game2D/src/com/Game2D/core/sound/SoundHandler.java
@@ -2,23 +2,75 @@ package com.Game2D.core.sound;
 
 import java.io.File;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Sound;
 
 public class SoundHandler {
 
 	private int amountOfSounds;
 	public Sound[] sounds = new Sound[amountOfSounds];
-	private String path = "/res/sounds";
+	private String[] fileNames;
+	public Sound currentlyPlaying;
+	public long soundID = 0;
 	
-	
-	public void getAmountOfSounds(String path) {
+	public void initSounds(String path) {
 		File folder = new File(path);
-		File[] files = folder.listFiles();
-		amountOfSounds = files.length;
+		fileNames = folder.list();
+		for(int i = 0; i < fileNames.length; i++) {
+			String temp = fileNames[i].toString();
+			sounds[i] = Gdx.audio.newSound(Gdx.files.internal(temp));
+		}
+		amountOfSounds = fileNames.length;
 	}
 	
-	public void load() {
-		getAmountOfSounds(path);
+	/**
+	 * @param sound Sound to play
+	 */
+	public void playSound(Sound sound) {
+		currentlyPlaying = sound;
+		soundID = sound.play();
+	}
+	
+	public void stopPlaying() {
+		if(currentlyPlaying != null)
+			currentlyPlaying.stop();
+	}
+	
+	/**
+	 * @param sound The sound to play
+	 * @param volume The volume to set (between 0, 1)
+	 */
+	public void setVolume(Sound sound, float volume) {
+		sound.setVolume(soundID, volume);
+	}
+	
+	/**
+	 * @param sound Sound to change pan and volume
+	 * @param pan Pan (-1 for Left, 1 for Right, 0 for center)
+	 * @param volume The volume to set (between 0, 1)
+	 */
+	public void setPan(Sound sound, float pan, float volume) {
+		sound.setPan(soundID, pan, volume);
+	}
+	
+	/**
+	 * @param sound Sound to set looping
+	 * @param flag true or false
+	 */
+	public void setLooping(Sound sound, boolean flag) {
+		sound.setLooping(soundID, flag);
+	}
+	
+	/**
+	 * @param sound Sound to change pitch
+	 * @param pitch <-1 Lower - 0 Normal - >1 Higher
+	 */
+	public void setPitch(Sound sound, float pitch) {
+		sound.setPitch(soundID, pitch);
+	}
+	
+	public void destroy(Sound sound) {
+		sound.dispose();
 	}
 	
 }
---
+++ Game2D/src/com/Game2D/core/util/LogHandler.java
@@ -0,0 +1,9 @@
+package com.Game2D.core.util;
+
+import java.util.logging.Logger;
+
+public class LogHandler {
+
+	public Logger GameLogger = Logger.getLogger(LogHandler.class.getName());
+	
+}
