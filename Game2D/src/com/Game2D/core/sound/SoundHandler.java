cfb7cb0ab2158f1f81a11ee5a5c215d40b772f2b
Anttoni <twistedmind123123@gmail.com>
2013-05-29 13:59:09 +0300

    Sound handler update / testing

--- Game2D/src/com/Game2D/core/sound/SoundHandler.java
+++ Game2D/src/com/Game2D/core/sound/SoundHandler.java
@@ -7,28 +7,43 @@ import com.badlogic.gdx.audio.Sound;
 
 public class SoundHandler {
 
+	/*
+	 * Todo:
+	 *  - Play multiple sounds at once
+	 *  - Sound fade -  out / fade - in
+	 */
+	
 	private int amountOfSounds;
 	public Sound[] sounds = new Sound[amountOfSounds];
 	private String[] fileNames;
 	public Sound currentlyPlaying;
 	public long soundID = 0;
-	
+	protected float volume;
+
 	public void initSounds(String path) {
 		File folder = new File(path);
 		fileNames = folder.list();
+		int count = 0;
 		for(int i = 0; i < fileNames.length; i++) {
 			String temp = fileNames[i].toString();
 			sounds[i] = Gdx.audio.newSound(Gdx.files.internal(temp));
+			count++;
 		}
 		amountOfSounds = fileNames.length;
+		System.out.println("Amount of sounds loaded: " + count);
 	}
 	
 	/**
 	 * @param sound Sound to play
 	 */
 	public void playSound(Sound sound) {
-		currentlyPlaying = sound;
-		soundID = sound.play();
+		if(currentlyPlaying == null) {
+			currentlyPlaying = sound;
+			soundID = sound.play();
+		} else if(currentlyPlaying != null) {
+			currentlyPlaying.stop();
+			soundID = sound.play();
+		}
 	}
 	
 	public void stopPlaying() {
@@ -42,6 +57,7 @@ public class SoundHandler {
 	 */
 	public void setVolume(Sound sound, float volume) {
 		sound.setVolume(soundID, volume);
+		this.volume = volume;
 	}
 	
 	/**
