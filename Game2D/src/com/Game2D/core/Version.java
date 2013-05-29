cfc5d6a546ec0711d7caa629832f3d80e3ebe484
Anttoni <twistedmind123123@gmail.com>
2013-05-29 21:07:57 +0300

    Version checker, only needs game updater

--- Game2D/src/com/Game2D/core/Version.java
+++ Game2D/src/com/Game2D/core/Version.java
@@ -1,7 +1,47 @@
 package com.Game2D.core;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class Version {
 
 	public static final String VERSION = "";
+	public static final String REMOTE_FILE = "";
+	
+	public void versionCheck() {
+		try {
+		
+			String loc = REMOTE_FILE;
+			HttpURLConnection conn = null;
+			while(loc != null && !loc.isEmpty()) {
+				URL url = new URL(loc);
+				conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestProperty("User-Agent",
+						"Mozilla/5.0 (Windows; U; Windows NT 6.0; ru; rv:1.9.0.11) Gecko/2009060215 Firefox/3.0.11 (.NET CLR 3.5.30729)");
+				conn.connect();
+				loc = conn.getHeaderField("Location");
+			}
+			
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+			String line = null;
+			
+			while((line = reader.readLine()) != null) {
+				if(line.startsWith(VERSION)) {
+					//Then this is the correct version
+				} else {
+					//Then this is not the correct version
+				}
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void update() {
+		
+	}
 	
 }
