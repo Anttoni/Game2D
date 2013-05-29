55f863f0ba878c809de5fae2218b8c3577600820
Anttoni <twistedmind123123@gmail.com>
2013-05-29 12:24:22 +0300

    Class renaming

--- Game2D/src/com/Game2D/core/GameMain.java
+++ Game2D/src/com/Game2D/core/GameMain.java
@@ -4,17 +4,16 @@ import com.Game2D.core.renderer.GameRenderer;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-public class GameScreen extends GameContainer {
+public class GameMain extends GameContainer {
 
 	public GameRenderer renderer = new GameRenderer();
 	
 	public static void main(String args[]) {
 	      LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
-	      	cfg.title = "Drop";
-	      	cfg.useGL20 = true;
+	      	cfg.title = "2D Roguelike Game";
+	      	cfg.useGL20 = false;
 	      	cfg.width = 800;
 	      	cfg.height = 480;
 	      new LwjglApplication(new GameContainer(), cfg);
 	}
-
 }
