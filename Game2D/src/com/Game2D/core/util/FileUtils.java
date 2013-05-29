a90912a1dd203c9cfe5db37bdf43ada3a2815bdb
Anttoni <twistedmind123123@gmail.com>
2013-05-29 21:44:32 +0300

    Fixed idiotic Issue ...

--- Game2D/src/com/Game2D/core/util/FileUtils.java
+++ Game2D/src/com/Game2D/core/util/FileUtils.java
@@ -3,16 +3,18 @@ package com.Game2D.core.util;
 import java.io.File;
 
 public class FileUtils {
-
+	
 	public static String[] listAllFiles(File file) {
 		return file.list();
 	}
+	
 	public static String withoutExtension(String fileName) {
 		String[] splitter = fileName.split(".");
 		return splitter[0];
 	}
+	
 	public static String getExtension(String fileName) {
 		String[] splitter = fileName.split(".");
-		return splitter[splitter.length - (splitter.length - 1)];
+		return splitter[splitter.length - 1];
 	}
 }
